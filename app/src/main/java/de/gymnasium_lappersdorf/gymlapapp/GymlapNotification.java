b7d4ef435c68961f42b2ed809130fd83a4ce8311
Schalk <SchalkLukas20@gmail.com>
2017-12-04 22:38:18 +0100

    Updated custom notification

--- app/src/main/java/de/gymnasium_lappersdorf/gymlapapp/GymlapNotification.java
+++ app/src/main/java/de/gymnasium_lappersdorf/gymlapapp/GymlapNotification.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 
@@ -14,21 +15,34 @@ import android.support.v4.app.NotificationCompat;
  */
 
 public class GymlapNotification {
+    private final static String CHANNEL_ID = "default_channel";
+    private static NotificationChannel sNotificationChannel;
+
     private NotificationManager mNotificationManager;
     private Notification mNotification;
     private int mUniqueId;
 
+    //One time initialization of the notification channel
+    static {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            sNotificationChannel = new NotificationChannel(CHANNEL_ID, "Default Channel", NotificationManager.IMPORTANCE_HIGH);
+        }
+    }
+
+    /**
+     * @param context     - basic context to create notification
+     * @param text        - text to be displayed in the notification
+     * @param resultClass - class to be executed on notification click
+     * @param id          - must be unique to identify the notification
+     */
     public GymlapNotification(Context context, String text, @Nullable Class<?> resultClass, int id) {
         mUniqueId = id;
-
-        String CHANNEL_ID = "default_channel";
 
         mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, "Default Channel", NotificationManager.IMPORTANCE_HIGH);
             if (mNotificationManager != null)
-                mNotificationManager.createNotificationChannel(mChannel);
+                mNotificationManager.createNotificationChannel(sNotificationChannel);
         }
 
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
@@ -37,7 +51,6 @@ public class GymlapNotification {
                 .setSmallIcon(R.drawable.ic_launcher_foreground);
 
         if (resultClass != null) {
-
             Intent resultIntent = new Intent(context, resultClass);
             PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
@@ -45,7 +58,6 @@ public class GymlapNotification {
         }
 
         mNotification = mBuilder.build();
-
     }
 
 
